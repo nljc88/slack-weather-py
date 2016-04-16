5336e030e242dbfca4cf23012f8169e0f359d067
Nathan Carlson <nathancarlson@me.com>
2016-04-15 20:53:46 -0400

    functiontest

--- weather.py
+++ weather.py
@@ -6,34 +6,34 @@ import datetime
 
 def get_weather_now():
 
-    f = urllib2.urlopen('http://api.wunderground.com/api/b32ef3297d82b842/geolookup/conditions/q/Ontario/Toronto.json')
-    json_string = f.read()
-    parsed_json = json.loads(json_string)
-    location = parsed_json['location']['city']
-    weathercondition = parsed_json['current_observation']['weather']
-    temp_c = parsed_json['current_observation']['temp_c']
-    feelslike_c = parsed_json['current_observation']['feelslike_c']
-    icon_url = parsed_json['current_observation']['icon_url']
-    forecast_url = parsed_json['current_observation']['forecast_url']
-    wind = parsed_json['current_observation']['wind_string']
-    rain = parsed_json['current_observation']['precip_today_metric']
+  f = urllib2.urlopen('http://api.wunderground.com/api/b32ef3297d82b842/geolookup/conditions/q/Ontario/Toronto.json')
+  json_string = f.read()
+  parsed_json = json.loads(json_string)
+  location = parsed_json['location']['city']
+  weathercondition = parsed_json['current_observation']['weather']
+  temp_c = parsed_json['current_observation']['temp_c']
+  feelslike_c = parsed_json['current_observation']['feelslike_c']
+  icon_url = parsed_json['current_observation']['icon_url']
+  forecast_url = parsed_json['current_observation']['forecast_url']
+  wind = parsed_json['current_observation']['wind_string']
+  rain = parsed_json['current_observation']['precip_today_metric']
 
-    rain_line = "We've had %smm of rain today" % (rain) if float(rain) > 0 else ''
-    bbq = 'FUCK YEAH BBQ TIME :hamburger: :beer:' if float(feelslike_c) > 15 and (weathercondition) == "Sunny" else ''
+  rain_line = "We've had %smm of rain today" % (rain) if float(rain) > 0 else ''
+  bbq = 'FUCK YEAH BBQ TIME :hamburger: :beer:' if float(feelslike_c) > 15 and (weathercondition) == "Sunny" else ''
 
-    now = datetime.datetime.now()
+  now = datetime.datetime.now()
 
-    url = "https://hooks.slack.com/services/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"
-    payload = {'username': "%s WeatherBot" % (now.strftime("%A")), 'text':"<%s|%s Weather>" % (forecast_url, location), 'icon_emoji': ":robot_face:", 'attachments':[
-          {
-             "fallback": "Weather Report",
-             "title":'%s' % (now.strftime("%A")),
-             "text": ('%s with a Temp of %s' + u"\u00B0" ' right meow' + '\nIt Really Feels like %s' + u"\u00B0" '\nExpect winds %s' + '\n%s') % (weathercondition, temp_c, feelslike_c, wind, rain_line) + bbq,
-             "color":"#D00000",
-             "thumb_url":'%s' % (icon_url),
-          }
-       ]}
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+  url = "https://hooks.slack.com/services/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"
+  payload = {'username': "%s WeatherBot" % (now.strftime("%A")), 'text':"<%s|%s Weather>" % (forecast_url, location), 'icon_emoji': ":robot_face:", 'attachments':[
+        {
+           "fallback": "Weather Report",
+           "title":'%s' % (now.strftime("%A")),
+           "text": ('%s with a Temp of %s' + u"\u00B0" ' right meow' + '\nIt Really Feels like %s' + u"\u00B0" '\nExpect winds %s' + '\n%s') % (weathercondition, temp_c, feelslike_c, wind, rain_line) + bbq,
+           "color":"#D00000",
+           "thumb_url":'%s' % (icon_url),
+        }
+     ]}
+  headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 r = requests.post(url, data=json.dumps(get_weather_now), headers=headers)   
 
